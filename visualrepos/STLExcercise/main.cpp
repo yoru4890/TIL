8aa3c342d3555ef73a49a09dbc3bd45fbc39388a
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-08-09 17:58:16 +0900

    [UPDATE] STL iterator, algorithm 0809

--- visualrepos/STLExcercise/main.cpp
+++ visualrepos/STLExcercise/main.cpp
@@ -1,39 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+struct Student
+{
+	int mNumber;
+	int mScore;
+	std::string mName;
+};
+
+std::ostream& operator << (std::ostream& os, std::vector<Student> v)
+{
+	for (auto it = v.begin(); it != v.end(); it++)
+	{
+		std::cout << it->mNumber << ", " << it->mScore << ", " << it->mName << std::endl;
+	}
+
+	return os;
+}
+
+bool CompareDescending(const Student* x, const Student& y)
+{
+	return x->mScore > y.mScore;
+}
 
 int main()
 {
-	std::vector<int> nums;
-	int num{}, sum{}, min{};
+	std::vector<Student> students{
+		{1, 100, "Doggy"},
+		{ 2, 50, "kitty" },
+		{ 3, 90, "Piggy" },
+		{ 4, 20 , "Bunny" }
+	};
 
-	while (std::cin >> num)
+	/*struct CompareAscending
 	{
-		nums.push_back(num);
-	}
-
-	min = num;
-
-	for (int n : nums)
-	{
-		sum += n;
-	}
-
-	for (int n : nums)
-	{
-		if (n < min)
+		bool operator() (Student x, Student y)
 		{
-			min = n;
+			return x.mScore < y.mScore;
 		}
-	}
+	};
 
-	std::cout << "합 : " << sum << std::endl;
+	CompareAscending c;
 
-	for (int i = 0; i < nums.size(); i++)
-	{
-		if (nums[i] == min)
+	std::sort(students.begin(), students.end(), c);*/
+	
+	//std::sort(students.begin(), students.end(), CompareDescending);
+
+	std::sort(students.begin(), students.end(), [](Student x, Student y)
 		{
-			std::cout << "최소값의 위치 : " << i << std::endl;
-			break;
-		}
-	}
+			return x.mScore < y.mScore;
+		});					
+
+	std::cout << students << std::endl;
+
+	return 0;
 }
