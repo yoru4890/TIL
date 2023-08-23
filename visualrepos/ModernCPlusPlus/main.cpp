4e98ec5c9f7064897cc8631e9b0283d4f8488937
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-08-23 17:58:30 +0900

    [UPDATE] ModrenCPlusPlus 0823

--- visualrepos/ModernCPlusPlus/main.cpp
+++ visualrepos/ModernCPlusPlus/main.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
-
-class Warrior
-{
-public:
-	int mHealth;
-	int mAttack;
-
-	Warrior(int h, int a) : mHealth{ h }, mAttack{ a }
-	{
-	}
-};
+#include <vector>
+#include <ranges>
 
 int main()
 {
-	std::unique_ptr<Warrior> spWarrior = std::make_unique<Warrior>(100, 20);
+	// C++ 20 사천왕 (big four)
+	// concept, range, coroutine, module
 
-	std::cout << spWarrior->mAttack << std::endl;
-
-	spWarrior.release();	// delete p;
-	spWarrior.reset();		// delete p; p = nullptr;
-
-	Warrior* ptr = spWarrior.get();	// 일반 포인터
+	
 }
 
