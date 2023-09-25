fa681fc8c3bf1eedebce6fb52de95e162ee48390
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-09-25 18:01:10 +0900

    [ADD] Windows_API 0925

---
+++ visualrepos/MyWindow/winmain.cpp
@@ -0,0 +1,88 @@
+#include <Windows.h>
+
+const wchar_t gClassName[] = L"MyWindowClass";
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+// entry point
+int WINAPI WinMain(_In_ HINSTANCE hInstance,
+					_In_opt_ HINSTANCE hPrevInstance,
+					_In_ LPSTR lpCmdLine,
+					_In_ int nShowCmd)
+{
+	// 1. '윈도우클래스' 등록
+	WNDCLASSEX wc{};
+	// ***importan***
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = gClassName;
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WindowProc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(nullptr, L"Failed to register", L"Error", MB_OK);
+		return 0;
+	}
+
+	// 2. '윈도우'를 생성
+	HWND hwnd;
+	hwnd = CreateWindowEx(
+		0,
+		gClassName,
+		L"Hello Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		640,
+		480,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (!hwnd)
+	{
+		MessageBox(nullptr, L"Failed to create", L"Error", MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
+
+	// 3. '윈도우 메시지' 처리
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+// 4. '윈도우 프로시저' 작성
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_CLOSE:
+			DestroyWindow(hWnd);
+			break;
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+
+		default:
+
+			return DefWindowProc(hWnd, message, wParam, lParam);
+			break;
+	}
+
+	return 0;
+}
