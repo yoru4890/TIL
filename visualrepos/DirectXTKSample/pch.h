6f79a61848c58f40ab143d08d7f125ae2249f105
yoru4890 <137769028+yoru4890@users.noreply.github.com>
2023-12-22 10:50:33 +0900

    [ADD] DirectXTK
    PreCompileHeader, StepTimer

---
+++ visualrepos/DirectXTKSample/pch.h
@@ -0,0 +1,101 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <winsdkver.h>
+#define _WIN32_WINNT 0x0A00
+#include <sdkddkver.h>
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+// DirectX apps don't need GDI
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+
+// Include <mcx.h> if you need this
+#define NOMCX
+
+// Include <winsvc.h> if you need this
+#define NOSERVICE
+
+// WinHelp is deprecated
+#define NOHELP
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+#include <wrl/client.h>
+
+#include <d3d11_1.h>
+#include <dxgi1_6.h>
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include <algorithm>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+
+#include <stdio.h>
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#pragma warning(push)
+#pragma warning(disable:26812)
+
+#include "Audio.h"
+#include "CommonStates.h"
+#include "WICTextureLoader.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PrimitiveBatch.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+
+#pragma warning(pop)
+
+namespace DX
+{
+	class com_exception : public std::exception
+	{
+	public:
+		com_exception(HRESULT hr) noexcept : result(hr) {}
+
+		const char* what() const override
+		{
+			static char s_str[64] = {};
+			sprintf_s(
+				s_str,
+				"Failure with HRESULT of %08X",
+				static_cast<unsigned int>(result)
+			);
+			return s_str;
+		}
+
+	private:
+		HRESULT result;
+	};
+		inline void ThrowIfFailed(HRESULT hr)
+		{
+			if (FAILED(hr))
+			{
+				throw com_exception(hr);
+			}
+		}
+
+}
